2a98c774f87749e3b9fe0da9b8b9fbb886689c97
michael.sevilla <michael.sevilla@cashpak.com>
2023-02-17 14:12:56 -0600

    prueba

--- CPPESTADOT/App_Start/BundleConfig.cs
+++ CPPESTADOT/App_Start/BundleConfig.cs
@@ -34,11 +34,11 @@ namespace CPPESTADOT
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                 "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                 "~/Content/bootstrap.css",
+                 "~/Content/bootstrap.min.css",
                  "~/Content/Site.css"));
         }
     }
