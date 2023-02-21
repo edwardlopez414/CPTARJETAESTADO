bb49ede846076052c28bfb437dca8c97c49c2208
Edward Enrique Lopez Perez <edward.lopez@info.grupocoen.com>
2023-02-21 17:31:32 -0600

    version ajax 1

--- CPPESTADOT/Controllers/HomeController.cs
+++ CPPESTADOT/Controllers/HomeController.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using CPPESTADOT.Models.API;
 
 namespace CPPESTADOT.Controllers
 {
@@ -11,6 +18,49 @@ namespace CPPESTADOT.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public async Task<JsonResult> API(string Variable)
+        {
+
+            bool status = false;
+            string respuesta = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+                //var usarname = "";
+                //var passwd = "";
+
+                client.BaseAddress = new Uri("https://www.banguat.gob.gt");
+
+
+                var intent = new APIDATA()
+                {
+
+                    Envelope = new Envelope()
+                    {
+                        Body = new Body()
+                        {
+                            Variables = new Variables()
+                            {
+                                variable = ""
+                            }
+                        }
+                    }
+                };
+                var json = JsonConvert.SerializeObject(intent);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("/variables/ws/TipoCambio.asmx", data);
+
+                status = response.IsSuccessStatusCode;
+
+                if (status) { 
+                respuesta = response.Content.ReadAsStringAsync().Result;
+                }
+
+                return Json(new { status = status, respuesta = respuesta }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
---
+++ CPPESTADOT/Models/API/APIDATA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CPPESTADOT.Models.API
+{
+    public class APIDATA
+    {
+        public Envelope Envelope { get; set; }
+    }
+
+    public class Body
+    {
+        public Variables Variables { get; set; }
+    }
+
+    public class Envelope
+    {
+        public Body Body { get; set; }
+    }
+
+    public class Variables
+    {
+        public string variable { get; set; }
+    }
+
+}
